0f4d655f801f2ff5ed13003581826ed8e5142f78
vgarciab <vgb.sw.gestion@gmail.com>
2021-03-30 11:27:54 +0200

    25 y 26. Nuevo Componente y haciendo Componentes reutilizables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,21 +7,29 @@ import { AppComponent } from './components/app/app.component';
 // Angular DatePipe - Cambiando el idioma.
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
+import { ListadoPeliculasComponent } from './components/peliculas/listado-peliculas/listado-peliculas.component';
 
 registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoPeliculasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [ {
-    provide: LOCALE_ID,
-    useValue: 'es'
-  }],
+  providers: [ 
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'EUR'
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -8,13 +8,14 @@ import { Component, Inject, OnInit, LOCALE_ID } from '@angular/core';
 export class AppComponent implements OnInit {
   
   title = 'front-end';
-
-  peliculas?:Array<{titulo:string, fechaLanzamiento: Date, precio:number}>;
+  peliculasEnCines?:Array<{titulo:string, fechaLanzamiento: Date, precio:number}>;
+  peliculasProximosEstrenos?:Array<{titulo:string, fechaLanzamiento: Date, precio:number}>;
+  // otra manera de declarar la variable >> peliculas: {titulo:string, fechaLanzamiento: Date, precio:number}[] | any;
 
   ngOnInit(): void {
     setTimeout(() => {
       // this.peliculas = [];
-      this.peliculas = [ 
+      this.peliculasEnCines = [ 
         {
           titulo: 'Spider-Man',
           fechaLanzamiento: new Date(),
@@ -26,7 +27,27 @@ export class AppComponent implements OnInit {
           fechaLanzamiento: new Date('2016-11-14'),
           precio: 300.99,
         }
+      ];
+
+      this.peliculasProximosEstrenos = [ 
+        {
+          titulo: 'Avengers: Endgame',
+          fechaLanzamiento: new Date(),
+          precio: 1400.99,
+        },
+        {
+          titulo: 'Inception',
+          fechaLanzamiento: new Date('2016-11-14'),
+          precio: 300.99,
+        },
+        {
+          titulo: 'Rocky',
+          fechaLanzamiento: new Date('2016-11-14'),
+          precio: 300.99,
+        }
       ]
+
+
     },2000);
   }
   
