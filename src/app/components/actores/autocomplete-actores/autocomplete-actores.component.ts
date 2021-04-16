31629d26c25738c7fbef0fdc3aeb188b7ba44ff2
vgarciab <vgb.sw.gestion@gmail.com>
2021-04-16 13:04:55 +0200

    67. Selector de actores - Arrastrando elementos

--- src/app/components/actores/autocomplete-actores/autocomplete-actores.component.ts
+++ src/app/components/actores/autocomplete-actores/autocomplete-actores.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
@@ -52,5 +53,15 @@ export class AutocompleteActoresComponent implements OnInit {
     this.actoresSeleccionados.splice(indice, 1);
     this.table.renderRows();
   }
+
+  finalizarArrastre(event: CdkDragDrop<any[]>) {
+    const indicePrevio = this.actoresSeleccionados.findIndex(
+      actor => actor === event.item.data
+    )
+    moveItemInArray(this.actoresSeleccionados, indicePrevio, event.currentIndex);
+    this.table.renderRows();
+  }
+  
+
 
 }
--- src/app/components/material/material.module.ts
+++ src/app/components/material/material.module.ts
@@ -13,6 +13,8 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatTableModule } from '@angular/material/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 
 
@@ -31,7 +33,8 @@ import { MatTableModule } from '@angular/material/table';
     MatNativeDateModule,
     MatTabsModule,
     MatAutocompleteModule,
-    MatTableModule
+    MatTableModule,
+    DragDropModule
   ],
   imports: [
     CommonModule
