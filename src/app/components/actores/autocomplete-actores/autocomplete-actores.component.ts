b1c7200f2643aa8a5d70a7061eb17a9c78367a51
vgarciab <vgb.sw.gestion@gmail.com>
2021-04-16 12:25:15 +0200

    66. Selector de actores - Componente Tabla

--- src/app/components/actores/autocomplete-actores/autocomplete-actores.component.ts
+++ src/app/components/actores/autocomplete-actores/autocomplete-actores.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-autocomplete-actores',
@@ -14,14 +15,16 @@ export class AutocompleteActoresComponent implements OnInit {
   // Un FormControl nos permite manejar un campo de un formulario de manera individual (en lugar de utilizar un FormGroup)
   control: FormControl = new FormControl();
   actores = [
-    {nombre: 'Tom Holland', foto: 'https://m.media-amazon.com/images/M/MV5BNzZiNTEyNTItYjNhMS00YjI2LWIwMWQtZmYwYTRlNjMyZTJjXkEyXkFqcGdeQXVyMTExNzQzMDE0._V1_UX214_CR0,0,214,317_AL_.jpg'},
-    {nombre: 'Tom Hanks', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg'},
-    {nombre: 'Samuel L. Jackson', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg'},
+    {nombre: 'Tom Holland', personaje: '', foto: 'https://m.media-amazon.com/images/M/MV5BNzZiNTEyNTItYjNhMS00YjI2LWIwMWQtZmYwYTRlNjMyZTJjXkEyXkFqcGdeQXVyMTExNzQzMDE0._V1_UX214_CR0,0,214,317_AL_.jpg'},
+    {nombre: 'Tom Hanks', personaje: '', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg'},
+    {nombre: 'Samuel L. Jackson', personaje: '', foto: 'https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg'},
   ]
 
   actoresOriginal = this.actores;
-
   actoresSeleccionados: any[] = []; // 'Tipado con 'any' porque si no, más abajo, en push(...), error:Argument of type 'any' is not assignable to parameter of type 'never' 
+  columnasAMostrar = ['imagen', 'nombre', 'personaje', 'acciones'];
+
+  @ViewChild(MatTable) table: MatTable<any> | any;
 
   ngOnInit(): void {
     // valueChanges es un Observable
@@ -38,6 +41,16 @@ export class AutocompleteActoresComponent implements OnInit {
     console.log(event.option.value);
     this.actoresSeleccionados.push(event.option.value);
     this.control.patchValue('');
+    if (this.table !== undefined) {
+      this.table.renderRows();
+    }
+  }
+
+
+  eliminar(actor: any) {
+    const indice = this.actoresSeleccionados.findIndex(a => a.nombre = actor.nombre);
+    this.actoresSeleccionados.splice(indice, 1);
+    this.table.renderRows();
   }
 
 }
--- src/app/components/material/material.module.ts
+++ src/app/components/material/material.module.ts
@@ -12,6 +12,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTableModule } from '@angular/material/table';
 
 
 
@@ -29,7 +30,8 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatDatepickerModule,
     MatNativeDateModule,
     MatTabsModule,
-    MatAutocompleteModule
+    MatAutocompleteModule,
+    MatTableModule
   ],
   imports: [
     CommonModule
